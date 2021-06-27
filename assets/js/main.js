7dba432bf8e34932290207715dae50568b399705
IRS-dev <irsyadalyahya5@gmail.com>
2021-06-27 17:04:28 +0700

    membuat preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
+// preloader
+jQuery(document).ready(function ($) {
+  $(window).load(function () {
+    setTimeout(function () {
+      $('#preloader').fadeOut('slow', function () {
+      });
+    }, 2000);
+
+  });
+});
+
+
 $(function () {
   ("use strict");
 
@@ -69,3 +81,4 @@ $(function () {
     }
   });
 });
+
